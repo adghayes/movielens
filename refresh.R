35f8a100963468089e060281b0e985af0d8eab16
adghayes <adghayes@gmail.com>
2019-10-06 20:57:40 -0700

    fixed refresh script to set wd

--- refresh.R
+++ refresh.R
@@ -1,5 +1,7 @@
-# running these four lines will reproduce the entire project
-source("dataset.R", echo = TRUE) # regenerage train and test sets
+# running this script will reproduce the entire project
+this.dir <- dirname(parent.frame(2)$ofile)
+setwd(this.dir)
+source("dataset.R", echo = TRUE) # regenerate train and test sets
 source("rmse.R", echo = TRUE) # rerun analysis
 rmarkdown::render("report.Rmd", "pdf_document") # reproduce PDF report
 rmarkdown::render("report.Rmd", "github_document", "README.md") # reproduce README.md
