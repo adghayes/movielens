8e4cbdd11c7486ae809e8bb120635b0ab43de64e
adghayes <adghayes@gmail.com>
2019-11-06 18:38:53 -0800

    beta of app

--- shiny/app.R
+++ shiny/app.R
@@ -1,1 +1,81 @@
 library(tidyverse)
+library(shiny)
+library(ggvis)
+library(data.table)
+load("./data.rda")
+n_movies <- nrow(raw_effects)
+
+ui <- fluidPage(
+  titlePanel("Regularization Explorer"),
+
+  sidebarPanel(
+    sliderInput(inputId = "lambda", 
+                label = "Lambda", 
+                value = 1, 
+                min = min(rmses_tbl$lambda), 
+                max = max(rmses_tbl$lambda),
+                step = lambda_step),
+    sliderInput(inputId = "top_n", 
+                label = "Top N (Highlight)", 
+                value = 25, 
+                min = 0, 
+                max = 500,
+                step = 5)
+  ),
+  mainPanel(
+    tabsetPanel(
+      tabPanel("Effect Plot", ggvisOutput("effects")),
+      tabPanel("Top Movies", tableOutput("top_movies"))
+    )
+  )
+ 
+)
+
+server <- function(input, output) {
+  
+  effects <- reactive({
+    f.effects(raw_effects, input$lambda) %>%
+      arrange(desc(effect)) %>%
+      mutate(ranking = row_number(),
+             rev_ranking = n_movies - ranking,
+             top = if_else(ranking <= input$top_n, "Yes", "No"))
+  })
+
+  
+  movie_tooltip <- function(x) {
+    if (is.null(x)) return(NULL)
+    
+    # Pick out the movie with this ID
+    movie <- effects[effects$movieId == x$movieId, ]
+    
+    paste0("<b>", movie$title, "</b><br>",
+           "Num. Ratings: " , movie$n, "<br>",
+           "Movie Effect:", movie$effect
+    )
+  }
+  
+  scatter <- reactive({
+    effects %>% 
+      ggvis(~log10(n), ~effect) %>%
+      layer_points(fill = ~top, size := 50, size.hover := 200, 
+                   fillOpacity := .2, fillOpacity.hover := .8) %>%
+      scale_ordinal("fill", range = c("gray", "purple")) %>%
+      add_axis("x", title = "Number of Ratings (Log. 10 Scale)") %>%
+      add_axis("y", title = "Average Rating") %>%
+      add_legend("fill", title = paste("Top ", input$top_n), values = c("No", "Yes"))
+      
+  })
+  
+  scatter %>% bind_shiny("effects")
+  
+  output$top_movies <-renderTable(effects() %>%
+    filter(ranking <= 20) %>%
+    arrange(ranking) %>%
+    select(Ranking = ranking, 
+           Title = title, 
+           `Number of Reviews` = n, 
+           `Average Rating` = effect))
+}
+
+shinyApp(ui = ui, server = server)
+
--- shiny/dataprep.R
+++ shiny/dataprep.R
@@ -4,49 +4,23 @@
 #################################################
 #################################################
 
-# Code for downloading and loading movielens
-# files are borrowed from edx course material
+library(tidyverse)
+library(caret)
+load("../data/movielens.rda")
 
-# Note: this process could take a couple of minutes
-
-if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
-if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
-if(!require(data.table)) install.packages("data.table", repos = "http://cran.us.r-project.org")
-
-# MovieLens 10M dataset:
-# https://grouplens.org/datasets/movielens/10m/
-# http://files.grouplens.org/datasets/movielens/ml-10m.zip
-
-dl <- tempfile()
-download.file("http://files.grouplens.org/datasets/movielens/ml-10m.zip", dl)
-
-ratings <- fread(text = gsub("::", "\t", readLines(unzip(dl, "ml-10M100K/ratings.dat"))),
-                 col.names = c("userId", "movieId", "rating", "timestamp")) %>%
-  select(movieId, rating)
-
-movies <- str_split_fixed(readLines(unzip(dl, "ml-10M100K/movies.dat")), "\\::", 3)
-colnames(movies) <- c("movieId", "title", "genres")
-movies <- as.data.frame(movies) %>% mutate(movieId = as.numeric(levels(movieId))[movieId],
-                                           title = as.character(title),
-                                           genres = as.character(genres))
-
-##################################
-####### Prepare App Data #########
-##################################
-ps <- seq(.1, .9, .1)
+lambda_min <- 0
+lambda_max <- 5
 lambda_step <- .01
-lambdas <- seq(3,3, by = lambda_step)
+lambdas <- seq(lambda_min, lambda_max, by = lambda_step)
 
 
 # Calculates Movie Effects w/o Regularization
-f.raw_effects <- function(ratings){
-  mu <- mean(ratings$rating)
-  ratings %>%
-    group_by(movieId) %>%
-    summarise(effect = mean(rating) - mu, n = n())
-}
+mu <- mean(edx$rating)
 
-raw_effects <- f.raw_effects(ratings)
+raw_effects <- edx %>%
+  group_by(movieId, title, genres) %>%
+  summarise(effect = mean(rating) - mu, n = n()) %>%
+  extract(title, c("title","year"), "(.*)\\s\\((\\d{4})\\)")
 
 # Adjusts Effects w/ Regularization
 f.effects <- function(raw_effects, lambda){
@@ -54,33 +28,16 @@ f.effects <- function(raw_effects, lambda){
     mutate(effect = effect*n/(n + lambda))
 }
 
-# For set of lambdas and single p, find RMSEs
-f.rmses <- function(ratings, lambdas, p){
-  set.seed(1, sample.kind="Rounding")
-  train_idx <- createDataPartition(y = ratings$rating, p = p, list = FALSE)
-  test <- ratings[-train_idx,]
-  train <- ratings[train_idx,]
-  train_mu <- mean(train$rating)
-  train_raw_effects <- f.raw_effects(train)
-  rmses <- sapply(lambdas, function(lambda){
-    train_reg_effects <- f.effects(train_raw_effects, lambda)
-    prediction <- test %>% 
-      left_join(train_reg_effects, by = "movieId") %>%
-      mutate(
-        prediction = if_else(!is.na(effect), 
-                  train_mu + effect, train_mu)) %>%
-      pull(prediction)
-      RMSE(prediction, test$rating)
-  })
-  return(tibble(p = p, lambda = lambdas, rmse = rmses))
-}
+# Find RMSEs for all lambdas
+rmses <- sapply(lambdas, function(lambda){
+  reg_effects <- f.effects(raw_effects, lambda)
+  prediction <- validation %>% 
+    left_join(reg_effects, by = "movieId") %>%
+    mutate(prediction = mu + effect) %>%
+    pull(prediction)
+  RMSE(prediction, validation$rating)
+})
 
-rmses_list <- lapply(ps, function(p){
-  f.rmses(ratings, lambda, p)
-  })
+rmses_tbl <- tibble(lambda = lambdas, rmse = rmses)
 
-rmses <- do.call(rbind, lapply(rmses_list, data.frame))
-
-
-save(movies, raw_effects, f.effects, rmse_dfs)
-
+save(mu, raw_effects, f.effects, rmses_tbl, lambda_min, lambda_max, lambda_step, file = "./data.rda")
